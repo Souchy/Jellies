0d68694679226a270d1b904968bbf7003e0d3355
Souchy <robyn.g@hotmail.fr>
2025-10-22 11:18:41 -0400

    Drag/Swap control good in BoardNode instead of PillNode
    TODO: Remove Area2D from PillNode?

--- game/board_node/BoardNode.cs
+++ game/board_node/BoardNode.cs
@@ -2,6 +2,7 @@ using Godot;
 using Godot.Sharp.Extras;
 using Jellies.game.pill_node;
 using Jellies.src;
+using Souchy.Godot.structures;
 using System;
 
 namespace Jellies.game.board_node;
@@ -11,24 +12,46 @@ public partial class BoardNode : Node2D
 
     #region Nodes
     [NodePath] public Node2D Pills { get; set; }
+    //[NodePath] 
+    public Area2D Area2D { get; set; }
     #endregion
 
     private Board Board { get; set; }
+    private TableArray<PillNode> PillNodesTable { get; set; }
     private PillNode? DraggingNode { get; set; }
+    private bool IsDragging => DraggingNode != null;
+    private Vector2 dragStartPosition;
 
     public override void _Ready()
     {
         this.OnReady();
+        this.Area2D = new Area2D()
+        {
+            InputPickable = true,
+            CollisionLayer = 1,
+            CollisionMask = 1
+        };
+        this.AddChild(Area2D);
+        this.Area2D.InputEvent += PillNode_InputEvent;
+        this.Area2D.MouseEntered += PillNode_MouseEntered;
+        this.Area2D.MouseExited += PillNode_MouseExited;
     }
 
     public void StartGame()
     {
+        // Clear data, nodes and shapes
         Board = BoardGenerator.Generate(difficulty: 1);
+        PillNodesTable = new(Board.pills.Width, Board.pills.Height, null);
         Pills.RemoveAndQueueFreeChildren();
+        PhysicsServer2D.AreaClearShapes(Area2D.GetRid());
+
+        // Offset board
         var halfBoardOffset = new Vector2(Board.pills.Size.X, Board.pills.Size.Y) * Constants.PillSize / 2f - Vector2.One * Constants.PillSize / 2f;
         this.Position = Vector2.Zero;
         Pills.Position = Vector2.Zero - halfBoardOffset;
+        Area2D.Position = Vector2.Zero - halfBoardOffset;
 
+        // New nodes
         foreach (var (i, j, pill) in Board.pills)
         {
             var sprite = pill.CreateNode();
@@ -36,44 +59,119 @@ public partial class BoardNode : Node2D
             pillnode.AddChild(sprite);
             pillnode.Position = new Vector2(i, j) * Constants.PillSize;
             pillnode.Board = this.Board;
-            pillnode.OnDrag += (node) =>
-            {
-                DraggingNode = pillnode;
-                DraggingNode.ZIndex = 10;
-            };
+            //pillnode.OnDrag += (node) =>
+            //{
+            //    DraggingNode = pillnode;
+            //    DraggingNode.ZIndex = 10;
+            //};
             pillnode.ZIndex = 0;
+            PillNodesTable[i, j] = pillnode;
             // pillnode.OnExit += (node) => {
             //     this.area.remove(node.shape); // TODO
             // };
+            // Create shape
+            var shapeRid = PhysicsServer2D.RectangleShapeCreate();
+            PhysicsServer2D.ShapeSetData(shapeRid, Vector2.One * Constants.PillSize / 2);
+            PhysicsServer2D.AreaAddShape(Area2D.GetRid(), shapeRid, new Transform2D(0, pillnode.Position));
             Pills.AddChild(pillnode);
+        }
+
+    }
+
+    private void PillNode_MouseEntered()
+    {
+        Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
+    }
+
+    private void PillNode_MouseExited()
+    {
+        if (!IsDragging)
+            Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
+    }
+
+    private void PillNode_InputEvent(Node viewport, InputEvent @event, long shapeIdx)
+    {
+        if (@event is InputEventMouseButton mouseClickEvent && mouseClickEvent.ButtonIndex == MouseButton.Left)
+        {
+            // On drag start
+            if (mouseClickEvent.Pressed && !IsDragging)
+            {
+                var grid2dPos = GetMouseGrid2DPos();
+                var gridPos = new Vector2I((int) grid2dPos.X, (int) grid2dPos.Y) / Constants.PillSize;
+                DraggingNode = PillNodesTable[gridPos];
+                dragStartPosition = DraggingNode.Position;
+                Input.SetDefaultCursorShape(Input.CursorShape.Drag);
+            }
         }
     }
 
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        if (DraggingNode?.isDragging != true)
+        if (!IsDragging)
             return;
-
-        if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left)
+        var lastPos = DraggingNode.Position;
+        var lastBoardPos = lastPos / Constants.PillSize;
+        bool wasSwapped = lastPos != dragStartPosition;
+        if (@event is InputEventMouseButton mouseClickEvent && mouseClickEvent.ButtonIndex == MouseButton.Left)
         {
-            if (!mouseEvent.Pressed)
+            // On drag release
+            if (!mouseClickEvent.Pressed && IsDragging)
             {
-                DraggingNode.isDragging = false;
+                // TODO: Check if there's a match and dont reset if so.
+                // reset both this node and the swapped node positions
+                DraggingNode.Position = dragStartPosition;
                 DraggingNode = null;
-                Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
+                if (wasSwapped)
+                {
+                    PillNodesTable[(int) lastBoardPos.X, (int) lastBoardPos.Y].Position = lastPos;
+                }
+                Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
             }
         }
         else
-        if (@event is InputEventMouseMotion mouseMotionEvent)
+        // On move 
+        if (@event is InputEventMouseMotion mouseMotionEvent && IsDragging)
         {
-            var boardMousePos = Pills.GetLocalMousePosition();
-            boardMousePos += Constants.PillVector / 2; // offset by center of pill ex: go from [-32,-32] to [0,0] and [32,32] becomes [64,64]
-            var cellLocalMousePos = boardMousePos % Constants.PillSize;
-            var coordPos = boardMousePos - cellLocalMousePos;
-            DraggingNode.Position = coordPos;
-            DraggingNode.ZIndex = 0;
+
+            var currPos = GetMouseGrid2DPos();
+            var delta = currPos - dragStartPosition;
+            delta = delta.Normalized();
+            bool horizontal = Mathf.Abs(delta.X) > Mathf.Abs(delta.Y);
+            if (horizontal)
+            {
+                delta = Mathf.Sign(delta.X) * Vector2.Right;
+            }
+            else
+            {
+                delta = Mathf.Sign(delta.Y) * Vector2.Down;
+            }
+            delta *= Constants.PillSize;
+            var newPos = dragStartPosition + delta;
+            var newBoardPos = newPos / Constants.PillSize;
+            int newX = (int) Mathf.Clamp(newBoardPos.X, 0, Board.pills.Width - 1);
+            int newY = (int) Mathf.Clamp(newBoardPos.Y, 0, Board.pills.Height - 1);
+            newBoardPos = new(newX, newY);
+            newPos = newBoardPos * Constants.PillSize;
+
+            // Preview swap
+            DraggingNode.Position = newPos;
+            PillNodesTable[(int) newBoardPos.X, (int) newBoardPos.Y].Position = dragStartPosition;
+            // if we were swapped with another node and we changed position, reset the other node's position
+            if (wasSwapped && lastPos != newPos)
+            {
+                PillNodesTable[(int) lastBoardPos.X, (int) lastBoardPos.Y].Position = lastPos;
+            }
         }
+    }
+
+    private Vector2 GetMouseGrid2DPos()
+    {
+        var boardMousePos = Pills.GetLocalMousePosition();
+        boardMousePos += Constants.PillVector / 2; // offset by center of pill ex: go from [-32,-32] to [0,0] and [32,32] becomes [64,64]
+        var cellLocalMousePos = boardMousePos % Constants.PillSize;
+        var coordPos = boardMousePos - cellLocalMousePos;
+        return coordPos;
     }
 
 }
--- game/pill_node/PillNode.cs
+++ game/pill_node/PillNode.cs
@@ -5,66 +5,26 @@ using System;
 
 namespace Jellies.game.pill_node;
 
-// TODO: 1 area with multiple shapes ? 
-// dont know if you can get the input event for each shape, 
-//  but then you can do all the click/drag inputs in BoardNode and get the pill from the mouse pos.
 public partial class PillNode : Area2D
 {
     public Board Board { get; set; }
 
     public int PillType { get; set; }
 
-    public bool isDragging;
-    private Vector2 _dragStartPosition;
-    public event Action<PillNode> OnDrag;
-    // public event Action<PillNode> OnExit;
-
     public override void _Ready()
     {
         base._Ready();
-        this.InputEvent += PillNode_InputEvent;
-        this.MouseEntered += PillNode_MouseEntered;
-        this.MouseExited += PillNode_MouseExited;
+        this.InputPickable = false;
     }
 
-    private void PillNode_MouseEntered()
-    {
-        Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
-    }
-
-    private void PillNode_MouseExited()
-    {
-        Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
-    }
-
-    private void PillNode_InputEvent(Node viewport, InputEvent @event, long shapeIdx)
-    {
-        if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left)
-        {
-            if (mouseEvent.Pressed)
-            {
-                if (!isDragging)
-                {
-                    Input.SetDefaultCursorShape(Input.CursorShape.Drag);
-                    isDragging = true;
-                    OnDrag(this);
-                }
-            }
-        }
-    }
-
-    public Vector2I GetGridPosition()
+    public Vector2I GetBoardPosition()
     {
         return new Vector2I((int) Position.X, (int) Position.Y) / Constants.PillSize;
     }
 
     public Pill GetPill()
     {
-        return Board.pills[GetGridPosition()];
+        return Board.pills[GetBoardPosition()];
     }
-
-    // public override void _ExitTree() {
-    //     this.OnExit(this);
-    // }
 
 }
